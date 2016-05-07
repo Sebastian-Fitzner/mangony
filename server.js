cbbb9b74ee1bdc8eecc8bc4b768021f8ddb57c57
Sebastian Fitzner <sebastian@jci-werbeagentur.com>
2016-05-07 07:34:00 +0200

    updated mangony server

---
+++ server.js
@@ -0,0 +1,43 @@
+var Mangony = require('./index');
+
+var mangony = new Mangony({
+	cwd: 'test/fixtures/',
+	dest: 'test/expected',
+	types: {
+		data: {
+			dir: 'data',
+			files: [
+				'/**/*.json',
+				'/**/*.hjson'
+			]
+		},
+		partials: {
+			dir: 'partials',
+			files: [
+				'/**/*.hbs'
+			]
+		},
+		pages: {
+			dir: 'pages',
+			files: [
+				'/**/*.hbs',
+				'/**/*.md'
+			]
+		},
+		layouts: {
+			dir: 'layouts',
+			files: [
+				'/**/*.hbs'
+			]
+		}
+	},
+	helpers: [
+		'helpers/*.js'
+	],
+	watch: true,
+	flatten: false,
+	compileStaticFiles: false,
+	useDevServer: true
+});
+
+mangony.render();
