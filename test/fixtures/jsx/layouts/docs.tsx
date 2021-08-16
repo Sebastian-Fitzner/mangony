aaeafbf1bc2ecf5d33321675daba1a3b0a6739f7
Sebastian Fitzner <sebastian.fitzner@axelspringer.com>
2021-08-16 08:37:37 +0200

    Add nested structure support for jsx templater

--- test/fixtures/jsx/layouts/docs.tsx
+++ test/fixtures/jsx/layouts/docs.tsx
@@ -1,11 +1,16 @@
+import * as fs from 'fs';
 import React, { Fragment } from 'react';
 import LytDefault from './lyt-default';
 
 function withLayoutDocs(cmp) {
     return LytDefault(({ root, context }) => {
+        const dirname = root.currentPage.dirname;
+        const readme = fs.readFileSync(`${dirname}/README.md`, 'utf-8');
+
         return (
             <Fragment>
                 <h1>Docs</h1>
+                {readme}
                 {
                     Object.values(context.variations).map((variation, idx) => {
                         return (
