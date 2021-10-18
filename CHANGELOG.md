96f03a65b0ba4dbe05d82d76593c1b4fdbc7cf18
Sebastian Fitzner <sebastian.fitzner@axelspringer.com>
2021-10-18 08:21:11 +0200

    Add type definitions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+### v2.0.0-alpha-13
+- Add type definitions for TS projects
+
 ### v2.0.0-alpha-12
 - Add ignore support
 - Remove buffer from cache
---
+++ types/mangony.d.ts
@@ -0,0 +1,53 @@
+export type Type<D> = {
+    id: string;
+    assets: string;
+    ext: string;
+    srcExt: string;
+    type: 'pages' | 'layouts' | 'partials' | 'commons' | unknown,
+    basename: string;
+    filename: string;
+    dirname: string;
+    destDir: string;
+    destSubDir: string;
+    destFile: string;
+    serverFile: string;
+    raw: string;
+    parsed: {
+        content: string;
+        data: D;
+        isEmpty: boolean;
+        excerpt: string;
+    }
+}
+
+export type Collection = {
+    name: string;
+    files: string[];
+}
+
+export type Root<D> = {
+    servermode: boolean;
+    __repository: Record<string, string[]>;
+    __layouts: Record<string, Type<D>>;
+    __partials: Record<string, Type<D>>;
+    pages: Record<string, Type<D>>;
+    collections: Record<string, Collection>;
+    [dataKey: string]: unknown;
+}
+
+export type PageSettings = {
+    contextData?: string;
+    layout?: string;
+    publish?: boolean;
+}
+
+export type ContextVariations<V = ContextVariation> = {
+    variations: Record<string, V>
+}
+
+export type ContextVariation = {
+    variationName: string;
+    variationDescription?: string;
+    docsSettings?: Record<string, unknown>;
+    props: unknown;
+}
